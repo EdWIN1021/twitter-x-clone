67e7f96b1ab14a9889169c1b284674ad72f4288a
Edwin <>
2023-11-29 20:26:00 -0800

    feat: change to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "react-loading-skeleton/dist/skeleton.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Root from "./pages/root";
 import Home from "./pages/home";
 import SignUp from "./pages/signup";
@@ -16,7 +16,7 @@ import RootLayout from "./layouts/RootLayout";
 
 import AuthProvider from "./contexts/AuthContext";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <RootLayout />,

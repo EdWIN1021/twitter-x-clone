fcded72ea8e8d456fb278b5fc403f4103d245b56
Edwin <>
2023-12-26 14:45:54 -0800

    fix: type

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -173,7 +173,7 @@ const PostForm = ({
                 { "opacity-50": !content },
               )}
             >
-              {type}
+              {type.slice(0, 1).toUpperCase() + type.slice(1, type.length)}
             </button>
           </div>
         </div>
